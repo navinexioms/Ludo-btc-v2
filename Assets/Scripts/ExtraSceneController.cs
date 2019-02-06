d4be17662e567e55abf1d50a98f2d66cd05540b6
Navin Dhuttargi <46275302+navinexioms@users.noreply.github.com>
2019-02-06 10:58:01 +0530

    This is newer Version of Ludo-btc
    This is the new Version of Ludo btc game .In this push One on One module is developed and this module is working fine. But waiting time is very much in next push have to reduce the waiting time for optimal gameplay

---
+++ Assets/Scripts/ExtraSceneController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class ExtraSceneController : MonoBehaviour 
+{
+	public static int HowManyPlayers;
+	public Toggle TwoPlayerToggle, FourPlayerToggle;
+	public GameObject TwoPlayerGameObject, FourPlayerGameObject;
+	public void SelectTwoPlayerGamePlay()
+	{
+		TwoPlayerGameObject.SetActive (TwoPlayerToggle.isOn);
+		FourPlayerGameObject.SetActive (false);
+	}
+	public void SelectFourPlayerGamePlay()
+	{
+		TwoPlayerGameObject.SetActive (false);
+		FourPlayerGameObject.SetActive (FourPlayerToggle.isOn);
+	}
+	public void LoadBettingAmountScene()
+	{
+		SceneManager.LoadScene ("BettingAmountScene");
+	}
+	public void LoadColorPickingScene()
+	{
+		SceneManager.LoadScene ("ColorPicking");
+	}
+	public void LoadExtraScene()
+	{
+		SceneManager.LoadScene ("ExtraScenes");
+	}
+	public void LoadProfileScene()
+	{
+		SceneManager.LoadScene ("ProfileScene");
+	}
+	public void WalletScene()
+	{
+		SceneManager.LoadScene ("WalletScene");
+	}
+	public void  LoadTransactionScene()
+	{
+		SceneManager.LoadScene ("TransactionScene");
+	}
+	public void SettingScene()
+	{
+		SceneManager.LoadScene ("SettingScene");
+	}
+	public void Logout()
+	{
+		SceneManager.LoadScene ("GameMenu");
+	}
+	public void SaveuserDataAndLoadExtraScene()
+	{
+		SceneManager.LoadScene ("ExtraScenes");
+	}	
+	public void LoadExtraSceneFromSetting()
+	{
+		SceneManager.LoadScene ("ExtraScenes");
+	}
+	public void LoadExtraScenefromTransaction()
+	{
+		SceneManager.LoadScene ("ExtraScenes");
+	}
+	public void LoadExtraSceneFromWalletScene()
+	{
+		SceneManager.LoadScene ("ExtraScenes");
+	}
+	public void LoadMainGameSceneFromExtraScene()
+	{
+		SceneManager.LoadScene ("GameMenu");
+	}
+	public void LoadPlayerVSAIScene()
+	{
+		HowManyPlayers = 2;
+		SceneManager.LoadScene ("PlayerVSAI");
+	}
+	public void LoadTwoPlayWithFriend()
+	{
+		SceneManager.LoadScene ("PlayWithFriend");
+	}
+	public void LoadOneOnOneScene()
+	{
+		SceneManager.LoadScene ("OneOnOneGameBoard");
+	}
+}
